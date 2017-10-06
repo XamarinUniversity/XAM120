11857b66959a1381df77b1f65dc4a76f213f23bd
Adam Patridge <patridge@gmail.com>
2017-10-06 12:17:52 -0600

    Update NuGet packages.

--- Exercise 3/Completed/Phoneword/Phoneword/MainPage.cs
+++ Exercise 3/Completed/Phoneword/Phoneword/MainPage.cs
@@ -12,30 +12,17 @@ namespace Phoneword
 
 		public MainPage()
 		{
-			this.Padding = new Thickness(20, Device.OnPlatform(40, 20, 20), 20, 20);
+            switch(Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    this.Padding = new Thickness(20, 40, 20, 20);
+                    break;
+                default:
+                    this.Padding = new Thickness(20);
+                    break;
+            }
 
-			// Optionally, if you want to experiment with using the ForPlatform.On system to
-			// differentiate between Windows 8 Silverlight (WinPhone) and newer Windows platforms
-			// (including UWP), here is an example to get you started.
-			//this.Padding = ForPlatform.On(
-			//	iOS: new Thickness(20, 40, 20, 20),
-			//	defaultValue: new Thickness(20)
-			//);
-
-			// Additionally, if you want to experiment with using the ForIdiom.On system to
-			// differentiate between different device form factors, here is an example to get you
-			// started.
-			//this.Padding = ForIdiom.On(
-			//	phone: new Thickness(20),
-			//	tablet: new Thickness(30),
-			//	desktop: new Thickness(10),
-			//	defaultValue: new Thickness(20)
-			//);
-
-			StackLayout panel = new StackLayout
-			{
-				Spacing = 15
-			};
+            StackLayout panel = new StackLayout { Spacing = 15 };
 
 			panel.Children.Add(new Label
 			{
@@ -65,7 +52,7 @@ namespace Phoneword
 			this.Content = panel;
 		}
 
-		private void OnTranslate(object sender, EventArgs e)
+		void OnTranslate(object sender, EventArgs e)
 		{
 			translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
 			if (!string.IsNullOrEmpty(translatedNumber))
@@ -80,18 +67,18 @@ namespace Phoneword
 			}
 		}
 
-		private async void OnCall(object sender, EventArgs e)
+		async void OnCall(object sender, EventArgs e)
 		{
-				if (await this.DisplayAlert(
-					"Dial a Number",
-					"Would you like to call " + translatedNumber + "?",
-					"Yes",
-					"No")) {
-					var dialer = DependencyService.Get<IDialer>();
-					if (dialer != null) {
-						await dialer.DialAsync(translatedNumber);
-					}
+			if (await this.DisplayAlert(
+				"Dial a Number",
+				"Would you like to call " + translatedNumber + "?",
+				"Yes",
+				"No")) {
+				var dialer = DependencyService.Get<IDialer>();
+				if (dialer != null) {
+					await dialer.DialAsync(translatedNumber);
 				}
+			}
 		}
 	}
 }
