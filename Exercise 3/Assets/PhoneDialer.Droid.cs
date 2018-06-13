6417ab6eb3c35098923c786da3d6764765e8fcd5
Adam Patridge <patridge@gmail.com>
2018-06-13 08:07:04 -0600

    Ex3: switch to Android context instead of Forms.

--- Exercise 3/Assets/PhoneDialer.Droid.cs
+++ Exercise 3/Assets/PhoneDialer.Droid.cs
@@ -12,14 +12,14 @@ namespace Phoneword.Droid
 {
     public class PhoneDialer : IDialer
     {
-    	/// <summary>
-    	/// Dial the phone
-    	/// </summary>
-		public Task<bool> DialAsync(string number)
-		{
-			var context = Forms.Context;
-    	    if (context != null)
-    	    {
+        /// <summary>
+        /// Dial the phone
+        /// </summary>
+        public Task<bool> DialAsync(string number)
+        {
+            var context = Android.App.Application.Context;
+            if (context != null)
+            {
                 var intent = new Intent(Intent.ActionCall);
                 intent.SetData(Uri.Parse("tel:" + number));
 
@@ -31,22 +31,22 @@ namespace Phoneword.Droid
             }
 
             return Task.FromResult(false);
-		}
+        }
 
         /// <summary>
         /// Checks if an intent can be handled.
         /// </summary>
-		public static bool IsIntentAvailable(Context context, Intent intent)
+        public static bool IsIntentAvailable(Context context, Intent intent)
         {
             var packageManager = context.PackageManager;
-            
-			var list = packageManager.QueryIntentServices(intent, 0)
-				.Union(packageManager.QueryIntentActivities(intent, 0));
-			if (list.Any())
-				return true;
-            
-			TelephonyManager mgr = TelephonyManager.FromContext(context);
-			return mgr.PhoneType != PhoneType.None;
+
+            var list = packageManager.QueryIntentServices(intent, 0)
+                .Union(packageManager.QueryIntentActivities(intent, 0));
+            if (list.Any())
+                return true;
+
+            TelephonyManager mgr = TelephonyManager.FromContext(context);
+            return mgr.PhoneType != PhoneType.None;
         }
     }
 }
--- Exercise 3/Completed/Phoneword/Phoneword.Droid/PhoneDialer.Droid.cs
+++ Exercise 3/Completed/Phoneword/Phoneword.Droid/PhoneDialer.Droid.cs
@@ -10,43 +10,43 @@ using Uri = Android.Net.Uri;
 
 namespace Phoneword.Droid
 {
-	public class PhoneDialer : IDialer
-	{
-		/// <summary>
-		/// Dial the phone
-		/// </summary>
-		public Task<bool> DialAsync(string number)
-		{
-			var context = Forms.Context;
-			if (context != null)
-			{
-				var intent = new Intent(Intent.ActionCall);
-				intent.SetData(Uri.Parse("tel:" + number));
+    public class PhoneDialer : IDialer
+    {
+        /// <summary>
+        /// Dial the phone
+        /// </summary>
+        public Task<bool> DialAsync(string number)
+        {
+            var context = Android.App.Application.Context;
+            if (context != null)
+            {
+                var intent = new Intent(Intent.ActionCall);
+                intent.SetData(Uri.Parse("tel:" + number));
 
-				if (IsIntentAvailable(context, intent))
-				{
-					context.StartActivity(intent);
-					return Task.FromResult(true);
-				}
-			}
+                if (IsIntentAvailable(context, intent))
+                {
+                    context.StartActivity(intent);
+                    return Task.FromResult(true);
+                }
+            }
 
-			return Task.FromResult(false);
-		}
+            return Task.FromResult(false);
+        }
 
-		/// <summary>
-		/// Checks if an intent can be handled.
-		/// </summary>
-		public static bool IsIntentAvailable(Context context, Intent intent)
-		{
-			var packageManager = context.PackageManager;
-			
-			var list = packageManager.QueryIntentServices(intent, 0)
-				.Union(packageManager.QueryIntentActivities(intent, 0));
-			if (list.Any())
-				return true;
-			
-			TelephonyManager mgr = TelephonyManager.FromContext(context);
-			return mgr.PhoneType != PhoneType.None;
-		}
-	}
+        /// <summary>
+        /// Checks if an intent can be handled.
+        /// </summary>
+        public static bool IsIntentAvailable(Context context, Intent intent)
+        {
+            var packageManager = context.PackageManager;
+
+            var list = packageManager.QueryIntentServices(intent, 0)
+                .Union(packageManager.QueryIntentActivities(intent, 0));
+            if (list.Any())
+                return true;
+
+            TelephonyManager mgr = TelephonyManager.FromContext(context);
+            return mgr.PhoneType != PhoneType.None;
+        }
+    }
 }
