d6bdc0135a6ede2b46ec6bffd520bf16b145d45e
Adam Patridge <patridge@gmail.com>
2018-08-21 12:04:26 -0600

    Ex3: update for Android runtime permissions.

--- Exercise 3/Assets/PhoneDialer.Droid.cs
+++ Exercise 3/Assets/PhoneDialer.Droid.cs
@@ -4,6 +4,8 @@ using Android.Content;
 using Android.Telephony;
 using Xamarin.Forms;
 using Uri = Android.Net.Uri;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
 
 namespace Phoneword.Droid
 {
@@ -12,22 +14,35 @@ namespace Phoneword.Droid
         /// <summary>
         /// Dial the phone
         /// </summary>
-        public Task<bool> DialAsync(string number)
+        public async Task<bool> DialAsync(string number)
         {
-            var context = Android.App.Application.Context;
-            if (context != null)
+            var permissionToCheck = Permission.Phone;
+            var status = await CrossPermissions.Current.CheckPermissionStatusAsync(permissionToCheck);
+            if (status != PermissionStatus.Granted)
             {
-                var intent = new Intent(Intent.ActionCall);
-                intent.SetData(Uri.Parse("tel:" + number));
+                var results = await CrossPermissions.Current.RequestPermissionsAsync(permissionToCheck);
+                //Best practice to always check that the key exists
+                if (results.ContainsKey(permissionToCheck))
+                    status = results[permissionToCheck];
+            }
 
-                if (IsIntentAvailable(context, intent))
+            if (status == PermissionStatus.Granted)
+            {
+                var context = Android.App.Application.Context;
+                if (context != null)
                 {
-                    context.StartActivity(intent);
-                    return Task.FromResult(true);
+                    var intent = new Intent(Intent.ActionCall);
+                    intent.SetData(Uri.Parse("tel:" + number));
+
+                    if (IsIntentAvailable(context, intent))
+                    {
+                        context.StartActivity(intent);
+                        return await Task.FromResult(true);
+                    }
                 }
             }
 
-            return Task.FromResult(false);
+            return await Task.FromResult(false);
         }
 
         /// <summary>
